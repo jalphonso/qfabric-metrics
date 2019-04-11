a340ebde4eba2aae25e048001dbaf712fdbd6f6a
Joe Alphonso <jalphonso@juniper.net>
2019-04-11 10:00:34 -0400

    added report

--- post/analyze_data.py
+++ post/analyze_data.py
@@ -9,12 +9,15 @@ csv_path = "csv"
 
 start = time.time()
 
+
 def average(num_list):
   return round(float(sum(num_list))/len(num_list),2)
 
 
 try:
   csv_files = [f for f in listdir(csv_path) if isfile(join(csv_path, f)) and os.path.splitext(f)[-1]=='.csv']
+  report = open("report.txt", 'w')
+  report.write("Interface        input_bps(min)       input_bps(max)       input_bps(avg)       output_bps(min)      output_bps(max)      output_bps(avg)\n")
   for filename in csv_files:
 
     #Intialize dataset to empty lists
@@ -40,17 +43,13 @@ try:
         interface = row[0]
         input_bps.append(int(row[11]))
         output_bps.append(int(row[12]))
-      print("Interface      input_bps(min)     input_bps(max)     input_bps(avg)      output_bps(min)     output_bps(max)      output_bps(avg)")
-      print("%15s %10.2f" % (interface, average(input_bps)))
-      #print(min(input_bps))
-      #print(average(input_bps))
-      #print(max(input_bps))
-      #print(min(output_bps))
-      #print(max(output_bps))
+      report.write("%-16s %-20.2f %-20.2f %-20.2f %-20.2f %-20.2f %-20.2f\n" % (interface, min(input_bps), max(input_bps), average(input_bps),  min(output_bps), max(output_bps), average(output_bps)))
     finally:
       f.close()
 except IOError:
   print("Failed to read file")
+finally:
+  report.close()
 
 end = time.time()
 print("Post processing took %s seconds to run" % (end - start))
